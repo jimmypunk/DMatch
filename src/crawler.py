c107ec540eb49500c07de3d20b59c28d0c09ef2e
王鈞奕 <jimmy@Jimmy-MacBook-Air.local>
2012-09-24 14:37:41 +0800

    this is a crawler which fetch any image from the url u type in usage: python crawler.py [url]

---
+++ src/crawler.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import urllib2
+import re
+import os
+import urlparse
+import sys
+def fetchImage(url):
+	htmlData = urllib2.urlopen(url).read()
+	soup = BeautifulSoup(htmlData)
+	imgs = soup.findAll("img")
+	for img in imgs:
+		imgUrl = urlparse.urljoin(url,img['src'])
+		print "downloading img" + imgUrl + "..."
+		cmd = "wget -P ./../res "+imgUrl
+		os.system(cmd)
+
+def main():
+	if(len(sys.argv) != 2):
+		print "[usage] python crawler.py url"
+		exit()
+	fetchImage(url)
+
+if __name__ =="__main__":
+	main()
