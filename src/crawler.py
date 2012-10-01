efd47f9d456394b50b66c3f05775c038c01f0115
janetyc <janetyc@gmail.com>
2012-10-01 09:52:29 +0800

    fixed an issue about input parameter

--- src/crawler.py
+++ src/crawler.py
@@ -4,21 +4,22 @@ import re
 import os
 import urlparse
 import sys
+
 def fetchImage(url):
-	htmlData = urllib2.urlopen(url).read()
-	soup = BeautifulSoup(htmlData)
-	imgs = soup.findAll("img")
-	for img in imgs:
-		imgUrl = urlparse.urljoin(url,img['src'])
-		print "downloading img" + imgUrl + "..."
-		cmd = "wget -P ./../res "+imgUrl
-		os.system(cmd)
+    htmlData = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(htmlData)
+    imgs = soup.findAll("img")
+    for img in imgs:
+        imgUrl = urlparse.urljoin(url,img['src'])
+        print "downloading img" + imgUrl + "..."
+        cmd = "wget -P ./../res "+imgUrl
+        os.system(cmd)
 
 def main():
-	if(len(sys.argv) != 2):
-		print "[usage] python crawler.py url"
-		exit()
-	fetchImage(url)
+    if(len(sys.argv) != 2):
+        print "[usage] python crawler.py url"
+        exit()
+    fetchImage(argv[1])
 
 if __name__ =="__main__":
 	main()
