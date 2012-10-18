9597260f06a9cee4499ec217bc412c7af142f85c
Jimmy Wang <wang.jimmy319@gmail.com>
2012-10-18 23:02:23 +0800

    change layoutSegmentation into a function, the parameter is the path

--- src/layoutSegmentation.m
+++ src/layoutSegmentation.m
@@ -1,5 +1,5 @@
-clear
-RGB = imread('url17.jpg');
+function boxList = layoutSegmentation(path, property)
+RGB = imread(path);
 I = rgb2gray(RGB);
 %figure, imshow(I), title('original image');
 text(size(I,2),size(I,1)+15, ...
@@ -8,16 +8,17 @@ text(size(I,2),size(I,1)+15, ...
 text(size(I,2),size(I,1)+25, ....
     'Johns Hopkins University', ...
     'FontSize',7,'HorizontalAlignment','right');
-[haha, threshold] = edge(I, 'sobel');
-fudgeFactor = .5;
-BWs = edge(I,'sobel', threshold * fudgeFactor);
+%[haha, threshold] = edge(I, 'sobel');
+%fudgeFactor = .5;
+%BWs = edge(I,'sobel', threshold * fudgeFactor);
+[BWs ,threshold] = edge(I,'canny');
 %figure, imshow(BWs), title('binary gradient mask');
 se90 = strel('line', 3, 90);
 se0 = strel('line', 3, 0);
 BWsdil = imdilate(BWs, [se90 se0]);
 %figure, imshow(BWsdil), title('dilated gradient mask');
 BWdfill = imfill(BWsdil, 'holes');
-figure, imshow(BWdfill),title('binary image with filled holes');
+%figure, imshow(BWdfill),title('binary image with filled holes');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 BWnobord = imclearborder(BWdfill, 4);
@@ -27,8 +28,9 @@ BWnobord = imclearborder(BWdfill, 4);
 seD = strel('diamond',1);
 BWfinal = imerode(BWnobord,seD);
 BWfinal = imerode(BWfinal,seD);
-figure, imshow(BWfinal), title('segmented image');
-figure, imshow(I), title('original image');
+%figure, imshow(BWfinal), title('segmented image');
+%figure, imshow(RGB), title('original image');
+figure, imshow(RGB), title('original image');
 hold on;
 
 %% draw boundingBox
@@ -43,15 +45,15 @@ end
 meanArea = mean(areaList);
 for i = 1:length(s)
    %if(areaList(i)>meanArea)
-      subImage = imcrop(I, s(i).BoundingBox);
-      %figure, imshow(subImage);
+      %subImage = imcrop(I, s(i).BoundingBox);
+      %%figure, imshow(subImage);
       rectangle('Position',s(i).BoundingBox,'EdgeColor','g');
    %end
 end
 hold off;
 %hist(areaList);
-BWoutline = bwperim(BWfinal);
-Segout = I;
-Segout(BWoutline) = 255;
-figure, imshow(Segout), title('outlined original image');
+%BWoutline = bwperim(BWfinal);
+%Segout = I;
+%Segout(BWoutline) = 255;
+%figure, imshow(Segout), title('outlined original image');
 
