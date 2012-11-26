c6ffc65ed825721cad2774899b8ede6526d21213
Jimmy Wang <wang.jimmy319@gmail.com>
2012-11-26 13:26:53 +0800

    change wget to urllib.urlretrieve so that windows can run it

--- src/crawler.py
+++ src/crawler.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib2
+import urllib
 import re
 import os
 import urlparse
@@ -9,16 +10,23 @@ def fetchImage(url):
     htmlData = urllib2.urlopen(url).read()
     soup = BeautifulSoup(htmlData)
     imgs = soup.findAll("img")
+    count = 0
     for img in imgs:
         imgUrl = urlparse.urljoin(url,img['src'])
         print "downloading img" + imgUrl + "..."
-        cmd = "wget -P ./../res "+imgUrl
-        os.system(cmd)
+        #cmd = "wget -P ./../res "+imgUrl
+        #os.system(cmd)
+        
+        urllib.urlretrieve(imgUrl, "./../res/url"+str(count)+".jpg")
+        count = count +1;
 
 def main():
 	if(len(sys.argv) != 2):
 		print "[usage] python crawler.py url"
 		exit()
+      
+
+
 	fetchImage(sys.argv[1])
 
 if __name__ =="__main__":
