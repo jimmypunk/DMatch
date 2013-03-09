41f7b32521432c77765f7157dd9d4dadf617428e
jimmypunk <wang.jimmy319@gmail.com>
2013-03-10 01:24:26 +0800

    add the app name and category information

--- src/crawler.py
+++ src/crawler.py
@@ -5,29 +5,54 @@ import re
 import os
 import urlparse
 import sys
+from Queue import Queue
+visited_url = []
+loaded_image_url = []
+container = Queue()
+def fetchImage(hostname):
 
-def fetchImage(url):
-    htmlData = urllib2.urlopen(url).read()
-    soup = BeautifulSoup(htmlData)
-    imgs = soup.findAll("img")
-    count = 0
-    for img in imgs:
-        imgUrl = urlparse.urljoin(url,img['src'])
-        print "downloading img" + imgUrl + "..."
-        #cmd = "wget -P ./../res "+imgUrl
-        #os.system(cmd)
+    while(container.empty() == False):
+        url,depth = container.get()
+        if url in visited_url or depth>3 or hostname!=urlparse.urlparse(url).hostname:
+            continue
+        visited_url.append(url)
+        print 'visiting '+url +' at depth:',depth
+        htmlData = urllib2.urlopen(url).read()
+        soup = BeautifulSoup(htmlData)
         
-        urllib.urlretrieve(imgUrl, "./../res/url"+str(count)+".jpg")
-        count = count +1;
-
+        #search imgs in this body section
+        
+        articles = soup.body("article")
+        count = 0
+        for article in articles:
+            if(article.img['src']==""):
+                continue
+            imgUrl = urlparse.urljoin(url,article.img['src'])
+            appName = article.section.h3.a.string
+            category = article.section.h3.span.string
+            if(imgUrl not in loaded_image_url):
+                print 'imgUrl:' + imgUrl + ' appName:' +appName.encode('big5','ignore') + ' category:'+category.encode('big5','ignore')+' found'
+                loaded_image_url.append(imgUrl)
+                count = count +1;
+                #print "downloading img:" + imgUrl + "	..."
+                #cmd = "wget -P ./../res "+imgUrl
+                #os.system(cmd)
+                #urllib.urlretrieve(imgUrl, "./../res/url"+str(count)+".jpg")
+        
+        #search for adjancy pages
+        links = soup('a')
+        for link in links:
+            if('href' in dict(link.attrs)):
+                newurl = urlparse.urljoin(url,link['href'])
+                container.put((newurl,depth+1))
+        
 def main():
-	if(len(sys.argv) != 2):
+    if(len(sys.argv) != 2):
 		print "[usage] python crawler.py url"
 		exit()
-      
-
-
-	fetchImage(sys.argv[1])
+    container.put((sys.argv[1],0))
+    hostname = urlparse.urlparse(sys.argv[1]).hostname
+    fetchImage(hostname)
 
 if __name__ =="__main__":
 	main()
